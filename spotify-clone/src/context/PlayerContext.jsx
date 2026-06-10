632c125facf11807050904135febb92a0a1ac422
Harshith kumar <harshith@Harshiths-MacBook-Air.local>
2026-06-10 10:48:00 +0530

    Update backend URL

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -9,7 +9,7 @@ const PlayerContextProvider = (props) => {
     const audioRef = useRef();
     const seekBg = useRef();
     const seekBar = useRef();
-    const url = 'http://localhost:4000'
+    const url = 'https://spotify-vm7x.onrender.com';
 
     const [songsData, setSongsData] = useState([]);
     const [albumsData,setAlbumData] = useState([]);
